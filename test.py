84dcf12c66b0b9eed401d544a8b5a50fcd1c19b2
aleksandrdyo <dyoalex@gmail.com>
2021-10-20 18:58:25 +0300

    add new test

---
+++ test.py
@@ -0,0 +1,2 @@
+def testing_passing():
+    assert(1, 2, 3) == (1, 2, 3)
---
+++ test/test_pytest.py
@@ -0,0 +1,9 @@
+def testing_passing():
+    assert(1, 2, 3) == (1, 2, 3)
+
+def testing_failing():
+    assert(1, 5, 3) != (5, 2, 3)
+
+def testing_failing():
+    assert(1, 2, 3) != (3, 2, 1)
+
