d4e3c8ab0a67bce679a1f4043ffbfeeb0b968bcf
aleksandrdyo <dyoalex@gmail.com>
2022-02-05 23:14:52 +0300

    add new script for arr

---
+++ lessons/arr_str_vac.py
@@ -0,0 +1,9 @@
+bjj=['kimura', 'berimbolo', 'delariva', 'armbar', 'kneebar', 'amerikano']
+
+bjj.append('spider')
+bjj.append('omoplata')
+bjj.extend(['qwe', 'asd'])
+
+for x in bjj:
+    print(x)
+
